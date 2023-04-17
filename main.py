372a19d117b182b1577da333708a1392c6703b66
Arash Hatami <hatamiarash7@gmail.com>
2023-04-17 20:18:40 +0330

    add toml output

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 import json
 from ruamel.yaml import YAML
 from pyfiglet import Figlet
+import toml
 
 f = Figlet(font='standard')
 
@@ -20,11 +21,12 @@ yaml = YAML(typ='safe')
 print(f.renderText('SSH Config Generator'))
 
 ips = []
-servers = {}
+servers_json = {}
+servers_toml = {}
 
 
-def _create_list(server, ip):
-    """Create a list of servers and IPs
+def _create_json(server, ip):
+    """Create a json list of servers and IPs
 
     The default format is: role-zone-provider-code
 
@@ -38,21 +40,45 @@ def _create_list(server, ip):
     name = server.split('-')
     role = name[0]
 
-    servers.update({'other': {}}) if 'other' not in servers else None
+    servers_json.update({'other': {}}) if 'other' not in servers_json else None
 
     if role in ['edge', 'monitoring', 'specto', 'prx']:
-        servers.update({role: {}}) if role not in servers else None
+        servers_json.update({role: {}}) if role not in servers_json else None
         zone = name[1]
         provider = name[2]
         code = name[3]
 
-        servers[role].update({zone: {}}) if zone not in servers[role] else None
-        servers[role][zone].update(
-            {provider: {}}) if provider not in servers[role][zone] else None
+        servers_json[role].update(
+            {zone: {}}) if zone not in servers_json[role] else None
+        servers_json[role][zone].update(
+            {provider: {}}) if provider not in servers_json[role][zone] else None
 
-        servers[role][zone][provider].update({code: ip})
+        servers_json[role][zone][provider].update({code: ip})
     else:
-        servers['other'].update({server: ip})
+        servers_json['other'].update({server: ip})
+
+
+def _create_toml(server, ip):
+    """Create a toml list of servers and IPs
+
+    The default format is: role-zone-provider-code
+
+    Args:
+        server (string): The server name
+        ip (string): The server IP
+    """
+
+    name = server.split('-')
+    role = name[0]
+    zone = name[1] if len(name) > 1 else 'a'
+    provider = name[2] if len(name) > 2 else 'a'
+    code = name[3] if len(name) > 3 else 'a'
+    key = zone + '-' + provider + '-' + code
+
+    servers_toml.update({role: {}}) if role not in servers_toml else None
+    servers_toml[role].update(
+        {key: {}}) if key not in servers_toml[role] else None
+    servers_toml[role][key] = ip
 
 
 def generate():
@@ -64,7 +90,8 @@ def generate():
     with (
         open(file=output, mode='w', encoding='UTF-8') as writer,
         open(file='ip-list.json', mode='w', encoding='UTF-8') as ip_list,
-        open(file='server-list.json', mode='w', encoding='UTF-8') as server_list,
+        open(file='server-list.json', mode='w', encoding='UTF-8') as server_json,
+        open(file='server-list.toml', mode='w', encoding='UTF-8') as server_toml,
         open(file=inputFile, mode='r', encoding='UTF-8') as stream
     ):
         writer.write("############################################ " + mode +
@@ -76,7 +103,8 @@ def generate():
         for server in out['servers']:
             ip = out['servers'][server]['main_ip']
 
-            _create_list(server, ip)
+            _create_json(server, ip)
+            _create_toml(server, ip)
 
             writer.write("Host " + server + "\n")
             writer.write("\tHostName " + ip + "\n")
@@ -110,7 +138,8 @@ def generate():
                     COUNTER_TINC = COUNTER_TINC+1
 
         json.dump(ips, ip_list)
-        json.dump(servers, server_list)
+        json.dump(servers_json, server_json)
+        toml.dump(servers_toml, server_toml)
 
     print("--> " + str(COUNTER) + " servers added" +
           " ( " + str(COUNTER_TINC) + " with Tinc IP )")
